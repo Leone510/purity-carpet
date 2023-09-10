5f8ec33846e50cb41ed614d88391cd4cf3796c72
Leone510 <meteorologsio@gmail.com>
2023-09-10 16:41:04 +0300

    remove redux

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Global } from './GlobalStyles/GlobalStyles';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Provider store={store}>
-
       <Global/>
       <App />
-
-    </Provider>
     
   </React.StrictMode>
 );
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { orderListReduser } from "./orderList/orderListReduser";
-
-const rootReduser = combineReducers({
-   orderList: orderListReduser,
-})
-
-export const store = createStore(rootReduser);
